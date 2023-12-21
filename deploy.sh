848de0cd04abd1e1561a36be36323fdeebead8c7
maheshkatta@3 <76720429+maheshkattta@users.noreply.github.com>
2023-12-21 09:10:42 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,11 @@ else
     kill -9 $PID
     echo "stopped $PID"
 fi
-echo "starting new jar file"
-echo $BUILD_NUMBER
-nohup java -javaagent:/home/mahi/newrelic/newrelic.jar -jar monkeystore-0.0.1-SNAPSHOT-$BUILD_NUMBER.jar > app.log 2>&1 &
+
+LATEST_JAR=$(ls -lhrrt | grep "\.jar$" | tail -1 | awk '{print $9}')
+echo "Latest jar file is: $LATEST_JAR"
+echo "Deploying new build"
+nohup java -javaagent:/home/mahi/newrelic/newrelic.jar -jar "/home/mahi/$LATEST_JAR" > app.log 2>&1 &
 NEW_PID=$(ps -ef | grep monkeystore | grep -v grep | awk '{print $2}')
 echo "New build started with $NEW_PID"
 sleep 5
