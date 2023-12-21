906de3b340e64abd15c535cff50684168087a10a
maheshkatta@3 <76720429+maheshkattta@users.noreply.github.com>
2023-12-21 08:59:16 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,9 @@ else
     echo "stopped $PID"
 fi
 echo "starting new jar file"
+echo $BUILD_NUMBER
 nohup java -javaagent:/home/mahi/newrelic/newrelic.jar -jar monkeystore-0.0.1-SNAPSHOT-$BUILD_NUMBER.jar > app.log 2>&1 &
+NEW_PID=$(ps -ef | grep monkeystore | grep -v grep | awk '{print $2}')
+echo "New build started with $NEW_PID"
+sleep 5
+ps -ef | grep "monkeystore-0.0.1"
